51e93dd9daca154f6f69eef5c4bfc0d392c7a531
Mar <mar_music@gmx.de>
2023-04-15 23:42:55 +0200

    Example level and guard go back mode

--- walking_guard.gd
+++ walking_guard.gd
@@ -2,10 +2,23 @@ extends Path2D
 
 
 @export var speed := 32.0
-
+@export var go_back := false
 
 @onready var _follow : PathFollow2D = %follow
+@onready var _guard : CharacterBody2D = %follow/guard
+
+var direction := 1.0
+
+func _ready():
+	_follow.loop = !go_back
 
 
 func _physics_process(delta : float) -> void:
-	_follow.progress += delta * speed
+	_follow.progress += direction * delta * speed
+	if _follow.progress_ratio >= 1.0 and go_back:
+		direction *= -1.0
+		_guard.rotation += PI
+	if _follow.progress_ratio <= 0.0 and go_back:
+		direction *= -1.0
+		_guard.rotation -= PI
+			
